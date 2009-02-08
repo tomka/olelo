1c09362c734e195d56737a31a74b9ec6478a7b76
Daniel Mendler <minad@slizer.hadiko.de>
2009-02-08 06:51:50 +0100

    Time.ago added

--- wiki.rb
+++ wiki.rb
@@ -28,6 +28,35 @@ class Time
   def format
     strftime('%d. %h %Y %H:%M')
   end
+
+  def ago
+    delta = (Time.now.to_i - to_i).floor / 60
+    if delta.abs <= (8724*60)
+      distance = Time.distance_of_time_in_words(delta)
+      "#{distance} ago on #{format}"
+    else
+      format
+    end
+  end
+
+  def self.distance_of_time_in_words(minutes)
+    case
+    when minutes < 1
+      "less than a minute"
+    when minutes < 50
+      minutes == 1 ? "1 minute" : "#{minutes} minutes"
+    when minutes < 90
+      "about one hour"
+    when minutes < 1080
+      "#{(minutes / 60).round} hours"
+    when minutes < 1440
+      "one day"
+    when minutes < 2880
+      "about one day"
+    else
+      "#{(minutes / 1440).round} days"
+    end
+  end
 end
 
 class String
