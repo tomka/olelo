900053aabe6416b991718bc0c68f3744e9ffb659
Daniel Mendler <minad@hyperion.hadiko.de>
2009-04-03 02:28:00 +0200

    more image info

--- plugins/engine/imageinfo.rb
+++ plugins/engine/imageinfo.rb
@@ -1,8 +1,11 @@
 Wiki::Plugin.define 'engine/imageinfo' do
+  require 'RMagick'
+
   Wiki::Engine.create(:imageinfo, :priority => 1, :layout => true, :cacheable => true) do
     accepts {|page| page.mime.mediatype == 'image' }
     output do |context|
       @page = context.page
+      @image = Magick::Image.from_blob(@page.content).first
       haml :imageinfo
     end
   end
