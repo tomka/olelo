7e37210a97b8428cdaea0fc07366afdae696db38
Daniel Mendler <minad@slizer.hadiko.de>
2009-07-28 23:27:19 +0200

    allow cache breaking

--- lib/rack/purge.rb
+++ lib/rack/purge.rb
@@ -8,9 +8,11 @@ module Rack
       request = Request.new(env)
       if request.GET.key?('purge')
         request.GET.delete('purge')
-        env['QUERY_STRING'] = Rack::Utils.build_query(request.GET)
-        env['HTTP_PRAGMA'] = 'no-cache'
-        env['HTTP_CACHE_CONTROL'] ||= 'no-cache'
+        env.update('QUERY_STRING' => Rack::Utils.build_query(request.GET),
+                   'HTTP_PRAGMA' => 'no-cache',
+                   'HTTP_CACHE_CONTROL' => 'no-cache',
+                   'rack-cache.allow_revalidate' => true,
+                   'rack-cache.allow_reload' => true)
       end
       @app.call(env)
     end
