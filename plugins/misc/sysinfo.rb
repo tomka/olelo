64da07e4fe21242298ae31ce5718ab6504ee1ef4
Daniel Mendler <minad@slizer.hadiko.de>
2009-08-10 01:15:35 +0200

    fix

--- plugins/misc/sysinfo.rb
+++ plugins/misc/sysinfo.rb
@@ -5,6 +5,7 @@ class Wiki::App
   get '/sysinfo' do
     GC.start
     @plugins = Plugin.plugins.values.sort {|a,b| a.name <=> b.name }
+    @disabled_plugins = Array === Config.disabled_plugins ? Config.disabled_plugins : []
     @memory = `ps -o rss= -p #{Process.pid}`.to_i / 1024
     haml :sysinfo
   end
