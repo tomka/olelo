1115a0fa948283bf1a4c1474a159d3335f4b90ab
Daniel Mendler <minad@slizer.hadiko.de>
2009-11-26 16:57:43 +0100

    image resize limiter

--- lib/wiki/utils.rb
+++ lib/wiki/utils.rb
@@ -154,6 +154,34 @@ module Wiki
       end
     end
   end
+
+  class Semaphore
+
+    def initialize(counter = 1)
+      @mutex = Mutex.new
+      @cond = ConditionVariable.new
+      @counter = counter
+    end
+
+    def enter
+      @mutex.synchronize do
+        @cond.wait(@mutex) if (@counter -= 1) < 0
+      end
+    end
+
+    def leave
+      @mutex.synchronize do
+        @cond.signal if (@counter += 1) <= 0
+      end
+    end
+
+    def synchronize
+      enter
+      yield
+    ensure
+      leave
+    end
+  end
 end
 
 module Kernel
--- plugins/engine/image.rb
+++ plugins/engine/image.rb
@@ -2,6 +2,11 @@ author      'Daniel Mendler'
 description 'Image rendering engine'
 require     'open3'
 
+setup do
+  cpu_count = `cat /proc/cpuinfo | grep processor | wc -l`.to_i rescue 1
+  $image_semaphore = Semaphore.new(cpu_count)
+end
+
 Engine.create(:image, :priority => 2, :layout => false, :cacheable => true) do
   def svg?(page); page.mime.to_s =~ /svg/; end
   def accepts?(page); page.mime.mediatype == 'image'; end
@@ -10,15 +15,17 @@ Engine.create(:image, :priority => 2, :layout => false, :cacheable => true) do
   def output(context)
     page = context.page
     if svg?(page) || context['geometry']
-      geometry = context['geometry']
-      cmd = 'convert -limit memory 1048576 -limit area 1048576 -limit map 1048576'
-      cmd << " -resize '#{context['geometry']}'" if geometry =~ /^(\d+)?x(\d+)?[%!<>]*$/
-      cmd << ' - PNG:-'
-      Open3.popen3(cmd) { |stdin, stdout, stderr|
-        stdin << page.content
-        stdin.close
-        stdout.read
-      }
+      $image_semaphore.synchronize do
+        geometry = context['geometry']
+        cmd = 'convert -limit memory 1048576 -limit area 1048576 -limit map 1048576'
+        cmd << " -resize '#{context['geometry']}'" if geometry =~ /^(\d+)?x(\d+)?[%!<>]*$/
+        cmd << ' - PNG:-'
+        Open3.popen3(cmd) { |stdin, stdout, stderr|
+          stdin << page.content
+          stdin.close
+          stdout.read
+        }
+      end
     else
       super
     end
