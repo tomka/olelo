cb337fbe5d6d33b301aa7ce5b2c8cd5fc9187bc1
Daniel Mendler <minad@slizer.hadiko.de>
2009-12-12 15:32:45 +0100

    shadow css added

--- plugins/engine/gallery/gallery.js
+++ plugins/engine/gallery/gallery.js
@@ -1,14 +1,6 @@
-function shadow(html) {
-    return '<div class="shadow-outer"><div class="shadow"><div class="shadow"><div class="shadow"><div class="shadow"><div class="shadow-inner">' +
-	html + '</div></div></div></div></div></div>';
-}
-
 $(function() {
-    $('.thumbs a').each(function() {
-	$(this).html(shadow($(this).html()));
-    });
     $('.thumbs a').click(function() {
-	$('.screen').html(shadow('<img src="' + this.href + '"/>'));
+	$('.screen').html('<img src="' + this.href + '"/>');
 	return false;
     });
 });
