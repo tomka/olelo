58188e29f6f92a0bd2b2d9863a9b1b4f5ad97456
minad <minad@hyperion.hadiko.de>
2010-01-10 20:32:06 +0100

    search fixed

--- plugins/search/grep.rb
+++ plugins/search/grep.rb
@@ -3,10 +3,10 @@ description 'Basic searching via grep'
 
 class Wiki::App
   get '/search' do
-    lines = repository.git_grep('-e', params[:pattern], '-i', repository.branch).split("\n")
+    lines = repository.git_grep('-z', '-e', params[:pattern], '-i', repository.branch).split("\n")
     @matches = {}
     lines.each do |line|
-      if line =~ /(.*)\:(.*)\:(.*)/
+      if line =~ /(.*)\:(.*)\0(.*)/
         (@matches[$2] ||= []) << $3
       end
     end
