2af00a78993fef76ddda73e0f2a1d2e45a261926
Daniel Mendler <minad@slizer.hadiko.de>
2010-04-22 14:04:27 +0200

    tree engine pagination

--- plugins/engine/tree.rb
+++ plugins/engine/tree.rb
@@ -5,7 +5,11 @@ dependencies 'engine/engine'
 Engine.create(:tree, :priority => 1, :layout => true, :cacheable => true) do
   def accepts?(resource); resource.tree?; end
   def output(context)
+    @page = context.params[:curpage].to_i
+    per_page = 20
     @tree = context.tree
+    @pages = @tree.children.size / per_page
+    @children = @tree.children[(@page * per_page) ... ((@page + 1) * per_page)].to_a
     haml :tree, :layout => false
   end
 end
