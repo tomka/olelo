6adf61918b1de66daab55d0c2350a09c691f0059
Daniel Mendler <minad@slizer.hadiko.de>
2010-05-06 23:58:51 +0200

    fix mime type middleware which modifies mime type to text/html for browsers which don't support application/xhtml+xml

--- lib/rack/degrade_mime_type.rb
+++ lib/rack/degrade_mime_type.rb
@@ -7,7 +7,7 @@ module Rack
 
     def call(env)
       status, header, body = @app.call(env)
-      if header['Content-Type'] =~ %r{application/xhtml+xml} &&
+      if header['Content-Type'] =~ %r{application/xhtml\+xml} &&
           !env['HTTP_ACCEPT'].to_s.split(',').map(&:strip).include?('application/xhtml+xml')
         header['Content-Type'] = 'text/html'
       end
