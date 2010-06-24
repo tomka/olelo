71ccaffb69b367b527774eb2cd60a6fb7ef459a4
minad <dmendler@wurzelteiler.de>
2010-06-24 14:12:15 +0200

    translate engine names, add index_page engine for missing index pages

--- plugins/engine/index_page.rb
+++ plugins/engine/index_page.rb
@@ -2,6 +2,18 @@ author      'Daniel Mendler'
 description 'Index page engine'
 dependencies 'engine/engine'
 
+# Engine which redirects to non-existing index page
+Engine.create(:create_index_page, :priority => 2, :layout => true) do
+  def accepts?(resource)
+    resource.tree? && !Page.find(resource.path/Config.index_page)
+  end
+
+  def output(context)
+    throw :redirect, (context.resource.path/Config.index_page).urlpath
+  end
+end
+
+# Engine which shows index page
 Engine.create(:index_page, :priority => 1, :layout => true) do
   def accepts?(resource)
     resource.tree? && Page.find(resource.path/Config.index_page)
