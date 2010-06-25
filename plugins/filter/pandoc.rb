265004e8d48a11568bdf8f94d444b895bf9820a8
minad <dmendler@wurzelteiler.de>
2010-06-25 04:40:02 +0200

    pandoc filter added

---
+++ plugins/filter/pandoc.rb
@@ -0,0 +1,16 @@
+author       'Daniel Mendler'
+description  'Pandoc filter'
+dependencies 'engine/filter'
+
+class Pandoc < Filter
+  def initialize(options)
+    super
+    @command = "pandoc --to=#{options[:to]} --from=#{options[:from]}"
+  end
+
+  def filter(content)
+    shell_filter(@command, content)
+  end
+end
+
+Filter.register :pandoc, Pandoc
