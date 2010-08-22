97bc01c3135f7c2b24cbc6d33167ff19ca882963
minad <mail@daniel-mendler.de>
2010-08-22 16:20:22 +0200

    fix

--- plugins/engine/download.rb
+++ plugins/engine/download.rb
@@ -4,7 +4,7 @@ dependencies 'engine/engine'
 Engine.create(:download) do
   def accepts?(page); !page.content.empty?; end
   def output(context)
-    name = context.page.root ? :root.t : context.page.name.gsub(/[^\w.\-_]/, '_')
+    name = context.page.root? ? :root.t : context.page.name.gsub(/[^\w.\-_]/, '_')
     context.response['Content-Disposition'] = %{attachment; filename="#{name}"}
     context.page.content
   end
